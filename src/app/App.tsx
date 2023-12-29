2168699b62284a7aee12dcb866329a26bab7f3ec
Steinbrecher Maxim <maximste@gmail.com>
2023-12-29 22:47:34 +0300

    Updated theme switcher component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,14 +6,11 @@ import AppRouter from './providers/router/ui/AppRouter';
 import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
-    const { theme, toggleTheme } = useTheme()
+    const { theme } = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar />
-            <button onClick={toggleTheme}>
-                theme
-            </button>
             <AppRouter />
         </div>
     );
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import cls from './Navbar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher/ui/ThemeSwitcher';
+
 
 interface NavbarProps {
     className?: string;
@@ -9,14 +11,19 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>
-                    Главная
-                </AppLink>
-                <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
-                    О сайте
-                </AppLink>
+            <ThemeSwitcher />
+            <div>
+                <div className={cls.links}>
+                    <AppLink theme={AppLinkTheme.SECONDARY} to={'/'}>
+                        Главная
+                    </AppLink>
+                    <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>
+                        О сайте
+                    </AppLink>
+                </div>
+                
             </div>
+            
         </div>
     );
 };
