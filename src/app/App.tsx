00481f7c277613a1661999faa18f0ca3731773cc
Steinbrecher Maxim <maximste@gmail.com>
2023-12-29 20:45:36 +0300

    Added navbar widget and AppLink component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,17 +3,17 @@ import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import AppRouter from './providers/router/ui/AppRouter';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
     const { theme, toggleTheme } = useTheme()
 
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
             <button onClick={toggleTheme}>
                 theme
             </button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
             <AppRouter />
         </div>
     );
