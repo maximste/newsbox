28fe4d421c0f982488410f1697e7a4c563fbed8e
Steinbrecher Maxim <maximste@gmail.com>
2024-01-04 16:33:29 +0300

    Small eslinter errors fixes

--- src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -1,8 +1,7 @@
-import { render } from "@testing-library/react";
-import React from "react";
-import { ReactNode } from "react";
-import { I18nextProvider } from "react-i18next";
-import i18nForTests from "shared/config/i18n/i18nForTests";
+import { render } from '@testing-library/react';
+import { ReactNode } from 'react';
+import { I18nextProvider } from 'react-i18next';
+import i18nForTests from 'shared/config/i18n/i18nForTests';
 
 // Оборачивает тестируемый компонент в обертку и добавляет необходимую конфигурацию для переводов
 export function renderWithTransaltion(component: ReactNode) {
@@ -10,6 +9,5 @@ export function renderWithTransaltion(component: ReactNode) {
         <I18nextProvider i18n={i18nForTests}>
             {component}
         </I18nextProvider>,
-    )
-
+    );
 }
