61caecb3ef7daf0e5fdd36369e86ee9ac8153bb6
Steinbrecher Maxim <maximste@gmail.com>
2024-01-13 21:51:27 +0300

    fixed tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -1,15 +1,14 @@
 import { fireEvent, screen } from '@testing-library/react';
-import React from 'react';
 import { Sidebar } from 'widgets/Sidebar/ui/Sidebar/Sidebar';
-import { renderWithTransaltion } from '../../../../shared/lib/tests/renderWithTranslation/renderWithTranslation';
+import { componentRender } from 'shared/lib/tests/componentRender/componentRender';
 
 describe('Sidebar', () => {
     test('Sidebar render', () => {
-        renderWithTransaltion(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('Sidebar toggle', () => {
-        renderWithTransaltion(<Sidebar />);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar-toggle')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
