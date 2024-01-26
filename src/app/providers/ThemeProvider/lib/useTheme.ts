a9969227b9b616ae6660c35f5f56b48af6570b0c
Steinbrecher Maxim <maximste@gmail.com>
2024-01-26 22:49:22 +0300

    fixed theme for modal

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -14,6 +14,7 @@ export function useTheme(): UseThemeResult {
     const toggleTheme = () => {
         const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK;
         setTheme(newTheme);
+        document.body.className = newTheme;
         // Сохраняем значение в localStorage
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
     };
